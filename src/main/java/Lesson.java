76b86068e15aa8a8aadf3e4aebbe48afba2c1e28
SMYU <sm.yusupov@mail.ru>
2019-10-17 18:00:12 +0300

    work comp

--- src/main/java/Lesson.java
+++ src/main/java/Lesson.java
@@ -21,46 +21,46 @@ public class Lesson {
     }
 
     public static void main(String[] args) {
-        new Lesson(1);
-        update();
-        JPanel jPanel = new JPanel();
-        jPanel.setLayout(null);
-        JButton button = new JButton("refresh");
-        button.setSize(100,30);
-        button.setLocation(430,300);
-        JLabel gImage = new JLabel(new ImageIcon("batman.jpg"));
-        gImage.setSize(400,450);
-        gImage.setLocation(0,-50);
-        jTextField = new JTextField();
-        jTextField.setSize(100,30);
-        jTextField.setLocation(430,100);
-      // text = jTextField.getText();
+//        new Lesson(1);
+//        update();
+//        JPanel jPanel = new JPanel();
+//        jPanel.setLayout(null);
+//        JButton button = new JButton("refresh");
+//        button.setSize(100,30);
+//        button.setLocation(430,300);
+//        JLabel gImage = new JLabel(new ImageIcon("batman.jpg"));
+//        gImage.setSize(400,450);
+//        gImage.setLocation(0,-50);
+//        jTextField = new JTextField();
+//        jTextField.setSize(100,30);
+//        jTextField.setLocation(430,100);
+//      // text = jTextField.getText();
+//
+//        //jLabel.setText(labelText);
+//        button.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//
+//                check(jTextField.getText());
+//            }
+//        });
+//
+//
+//
+//        jLabel.setSize(100,50);
+//        jLabel.setLocation(430,10);
+//        jFrame = getFrame();
+//
+//        jPanel.add(button);
+//        jPanel.add(jLabel);
+//        jPanel.add(gImage);
+//        jPanel.add(jTextField);
+//        jFrame.add(jPanel);
 
-        //jLabel.setText(labelText);
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-
-                check(jTextField.getText());
-            }
-        });
 
 
-
-        jLabel.setSize(100,50);
-        jLabel.setLocation(430,10);
-        jFrame = getFrame();
-
-        jPanel.add(button);
-        jPanel.add(jLabel);
-        jPanel.add(gImage);
-        jPanel.add(jTextField);
-        jFrame.add(jPanel);
-
-
-
-//        JFrame myWindow = new SimpleWindow();
-//        myWindow.setVisible(true);
-
+        JFrame myWindow = new SimpleWindow(1);
+        myWindow.setVisible(true);
+        SimpleWindow.update();
     }
 
     static JFrame getFrame() {
@@ -73,45 +73,45 @@ public class Lesson {
         return jFrame;
     }
 
-    static void update() {
-        x  = (int) (Math.random() * 10);
-        y =  (int) (Math.random() * 10);
-        String labelText = x + " * " +  y;
-        jLabel.setText(labelText);
-    }
-
-    static void check(String text){
-        int a = Integer.parseInt(text);
-        if (a == x * y){
-            scope ++;
-            if (scope <= total){
-                icon = new ImageIcon("ok.jpg");
-                JOptionPane.showMessageDialog  (new JFrame(),"Ты молодец!", "", 1, icon);
-                jTextField.setText("");
-                jTextField.grabFocus();
-                update();
-
-            }
-            else {
-                icon = new ImageIcon("end.jpg");
-               int result = JOptionPane.showConfirmDialog (new JFrame(), "Еще одну битву?",
-                       "Конец битвы!", JOptionPane.YES_NO_OPTION, 2, icon);
-               if (result == JOptionPane.YES_OPTION){
-                   jTextField.setText("");
-                   jTextField.grabFocus();
-                   scope = 0;
-                   update();
-               } else {
-                  jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                  jFrame.dispose();
-                  System.exit(0);
-               }
-            }
-        } else {
-            icon = new ImageIcon("wrong2.jpg");
-            JOptionPane.showMessageDialog  (new JFrame(),"Попробуй еще раз!", "Не правильно!", 0, icon);
-            jTextField.setText("");
-            jTextField.grabFocus();
-        }
-    }
+//    static void update() {
+//        x  = (int) (Math.random() * 10);
+//        y =  (int) (Math.random() * 10);
+//        String labelText = x + " * " +  y;
+//        jLabel.setText(labelText);
+//    }
+//
+//    static void check(String text){
+//        int a = Integer.parseInt(text);
+//        if (a == x * y){
+//            scope ++;
+//            if (scope <= total){
+//                icon = new ImageIcon("ok.jpg");
+//                JOptionPane.showMessageDialog  (new JFrame(),"Ты молодец!", "", 1, icon);
+//                jTextField.setText("");
+//                jTextField.grabFocus();
+//                update();
+//
+//            }
+//            else {
+//                icon = new ImageIcon("end.jpg");
+//               int result = JOptionPane.showConfirmDialog (new JFrame(), "Еще одну битву?",
+//                       "Конец битвы!", JOptionPane.YES_NO_OPTION, 2, icon);
+//               if (result == JOptionPane.YES_OPTION){
+//                   jTextField.setText("");
+//                   jTextField.grabFocus();
+//                   scope = 0;
+//                   update();
+//               } else {
+//                  jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//                  jFrame.dispose();
+//                  System.exit(0);
+//               }
+//            }
+//        } else {
+//            icon = new ImageIcon("wrong2.jpg");
+//            JOptionPane.showMessageDialog  (new JFrame(),"Попробуй еще раз!", "Не правильно!", 0, icon);
+//            jTextField.setText("");
+//            jTextField.grabFocus();
+//        }
+//    }
 }
--- src/main/java/SimpleWindow.java
+++ src/main/java/SimpleWindow.java
@@ -1,21 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleWindow extends JFrame {
+   private JPanel panel;
+   private JButton button;
+   private JLabel jImage;
+   static int total;
+   static int x;
+   static int y;
+    static  int scope;
+    static JTextField jTextField;
+    static JLabel jLabel = new JLabel();
+    static JFrame jFrame;
+    static Icon icon;
 
-    SimpleWindow(){
+    SimpleWindow(int total){
         super("Пробное окно");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(null);
-        JButton button = new JButton("Кнопка");
+        this.total = total;
+        button = new JButton("Кнопка");
         button.setSize(80, 30);
         button.setLocation(20,20);
         panel.add(button);
         button = new JButton("Кнопка с длинной надписью");
         button.setSize(120, 40);
         button.setLocation(70,50);
-        JLabel jImage = new JLabel(new ImageIcon("batman.jpg"));
+        jImage = new JLabel(new ImageIcon("batman.jpg"));
         jImage.setSize(300,150);
         jImage.setLocation(150,100);
         panel.add(button);
@@ -24,6 +38,55 @@ public class SimpleWindow extends JFrame {
         setSize(250, 150);
         setBounds(500,500,750,400);
 
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                check(jTextField.getText());
+            }
+        });
+
+    }
+
+    static void update() {
+        x  = (int) (Math.random() * 10);
+        y =  (int) (Math.random() * 10);
+        String labelText = x + " * " +  y;
+        jLabel.setText(labelText);
+    }
+
+    static void check(String text){
+        int a = Integer.parseInt(text);
+        if (a == x * y){
+            scope ++;
+            if (scope <= total){
+                icon = new ImageIcon("ok.jpg");
+                JOptionPane.showMessageDialog  (new JFrame(),"Ты молодец!", "", 1, icon);
+                jTextField.setText("");
+                jTextField.grabFocus();
+                update();
+
+            }
+            else {
+                icon = new ImageIcon("end.jpg");
+                int result = JOptionPane.showConfirmDialog (new JFrame(), "Еще одну битву?",
+                        "Конец битвы!", JOptionPane.YES_NO_OPTION, 2, icon);
+                if (result == JOptionPane.YES_OPTION){
+                    jTextField.setText("");
+                    jTextField.grabFocus();
+                    scope = 0;
+                    update();
+                } else {
+                    jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    jFrame.dispose();
+                    System.exit(0);
+                }
+            }
+        } else {
+            icon = new ImageIcon("wrong2.jpg");
+            JOptionPane.showMessageDialog  (new JFrame(),"Попробуй еще раз!", "Не правильно!", 0, icon);
+            jTextField.setText("");
+            jTextField.grabFocus();
+        }
     }
 
 }
